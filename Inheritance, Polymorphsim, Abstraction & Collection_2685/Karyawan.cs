4e0b1a492d6096d7774ef546acd7c28041aba9c5
Deny Agung Prasetyo <58355447+Tyo2685@users.noreply.github.com>
2020-06-23 13:17:48 +0700

    Inheritance, Polymorphsim, Abstraction & Collection

--- Inheritance, Polymorphsim, Abstraction & Collection_2685/Karyawan.cs
+++ Inheritance, Polymorphsim, Abstraction & Collection_2685/Karyawan.cs
@@ -9,7 +9,7 @@ namespace Inheritance__Polymorphsim__Abstraction___Collection_2685
     public abstract class Karyawan
     {
         public string Nik{ get; set; }
-        public string Nama { get; set; }
+        public string Nama { get; set; 
         public abstract double Gaji();
     }
 }
